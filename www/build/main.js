fb5d49fe65e223aeaae6b5723f8ecb6452724687
Radhouane Ben Ncir <hackathonSofrecom2017@gmail.com>
2017-09-24 01:44:23 +0100

    merge fix

--- www/build/main.js
+++ www/build/main.js
@@ -108,10 +108,9 @@ var SpecialistServiceProvider = (function () {
 }());
 SpecialistServiceProvider = __decorate([
     Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["B" /* Injectable */])(),
-    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1__angular_http__["b" /* Http */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1__angular_http__["b" /* Http */]) === "function" && _a || Object])
+    __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__angular_http__["b" /* Http */]])
 ], SpecialistServiceProvider);
 
-var _a;
 //# sourceMappingURL=specialist-service.js.map
 
 /***/ }),
